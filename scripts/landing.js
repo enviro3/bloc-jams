ddea41aca75a8da134fe31b2dd2558185b0c6a8c
Caroline Vurlumis <cvurlumis@gmail.com>
2017-01-08 14:08:23 -0800

    Assignment 8 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,7 +2,24 @@
  
             var points = document.getElementsByClassName('point');
  
-            var revealFirstPoint = function() {
+            
+            var revealPoint = function(indexOfPoint){  //replaces var revealFirstPoint = function() for all three points 
+                
+                points[indexOfPoint].style.opacity = 1;
+                points[indexOfPoint].style.transform = "scaleX(1) translateY(0)";
+                points[indexOfPoint].style.msTransform = "scaleX(1) translateY(0)";
+                points[indexOfPoint].style.WebkitTransform = "scaleX(1) translateY(0)";
+            };
+            
+            for(var i=0; i<points.length; i++){ //replaces revealFirstPoint();...etc. 
+                revealPoint(i);
+            }
+      
+  };
+            
+                
+      
+           /* var revealFirstPoint = function() {
                 points[0].style.opacity = 1;
                 points[0].style.transform = "scaleX(1) translateY(0)";
                 points[0].style.msTransform = "scaleX(1) translateY(0)";
@@ -29,3 +46,4 @@
            
  
              };
+             */
