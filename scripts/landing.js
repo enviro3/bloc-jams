609a1c930b502a42066b6f656c4f361c6bd425a6
Caroline Vurlumis <cvurlumis@gmail.com>
2017-01-09 21:37:25 -0800

    Checkpoint 9 DOM Scripting Events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,22 +1,32 @@
-  var animatePoints = function() {
+ var pointsArray = document.getElementsByClassName('point');
  
-            var points = document.getElementsByClassName('point');
- 
+ var animatePoints = function(points) {
+    var revealPoint = function(indexOfPoint){  //replaces var revealFirstPoint = function() for all three points         
+        points[indexOfPoint].style.opacity = 1;
+        points[indexOfPoint].style.transform = "scaleX(1) translateY(0)";
+        points[indexOfPoint].style.msTransform = "scaleX(1) translateY(0)";
+        points[indexOfPoint].style.WebkitTransform = "scaleX(1) translateY(0)";
+    };
             
-            var revealPoint = function(indexOfPoint){  //replaces var revealFirstPoint = function() for all three points 
-                
-                points[indexOfPoint].style.opacity = 1;
-                points[indexOfPoint].style.transform = "scaleX(1) translateY(0)";
-                points[indexOfPoint].style.msTransform = "scaleX(1) translateY(0)";
-                points[indexOfPoint].style.WebkitTransform = "scaleX(1) translateY(0)";
-            };
-            
-            for(var i=0; i<points.length; i++){ //replaces revealFirstPoint();...etc. 
-                revealPoint(i);
-            }
+    for(var i=0; i<points.length; i++){ //replaces revealFirstPoint();...etc. 
+        revealPoint(i);
+    }
       
-  };
+};
             
                 
       
-       
+window.onload = function(){
+    
+    if (window.innerHeight > 950){
+        animatePoints(pointsArray);
+    }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+     window.addEventListener('scroll', function(event) {
+          if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+     });
+ }
