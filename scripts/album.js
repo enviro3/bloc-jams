a641d0682cb445dc097b78056345ed096605b965
Caroline Vurlumis <cvurlumis@gmail.com>
2017-01-21 21:29:39 -0800

    Checkpoint 13 fixed

--- scripts/album.js
+++ scripts/album.js
@@ -134,7 +134,6 @@ var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause">
          //only target individual song rows during event del
          if(event.target.parentElement.className === 'album-view-song-item') {
              //change the content from the number to the play button's HTML
-             event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
              var songItem = getSongItem(event.target);
              if(songItem.getAttribute('data-song-number') !== currentlyPlayingSong){
                  songItem.innerHTML = playButtonTemplate;
