1a00c1714398abdd1349ef781b66d3f565365eb7
Caroline Vurlumis <cvurlumis@gmail.com>
2017-01-29 12:16:05 -0800

    Checkpoint 17 jqueryCollection: complete

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,5 @@
-var collectionItemTemplate =
+ var buildCollectionItemTemplate = function() {
+     var template =
     '<div class="collection-album-container column fourth">'
    + '  <img src="assets/images/album_covers/01.png"/>'
    + '  <div class="collection-album-info caption">'
@@ -13,17 +14,22 @@ var collectionItemTemplate =
    + '  </div>'
    + '</div>';
 
- window.onload = function() {
+
+return $(template);
+ };
+
+ $(window).load(function() {
      // #1
-     var collectionContainer = document.getElementsByClassName('album-covers')[0];
-     // #2
-     collectionContainer.innerHTML = '';
+     var $collectionContainer = $('.album-covers');
+     
+     $collectionContainer.empty();
  
      // #3
      for (var i = 0; i < 12; i++) {
-         collectionContainer.innerHTML += collectionItemTemplate;
+          var $newThumbnail = buildCollectionItemTemplate();
+          $collectionContainer.append($newThumbnail);
      }
- }
+ });
     
     
     
